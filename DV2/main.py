f5e26620a013487480f8777b21aa80a1d95228e3
mamakonatef26 <mamakonatef@gmail.com>
2026-04-15 11:02:02 +0000

    Update main.py

--- DV2/main.py
+++ DV2/main.py
@@ -1,9 +1,20 @@
 import streamlit as st
 import streamlit.components.v1 as components
+import os
 
 st.set_page_config(layout="wide")
 st.title("Cartographie des Instituts et Réseaux au Sénégal")
 
-with open("index.html", 'r', encoding='utf-8') as f:
-    html_data = f.read()
-    components.html(html_data, height=700)
+file_name = "index.html"
+
+if os.path.exists(file_name):
+    path = file_name
+else:
+    path = os.path.join("DV2", file_name)
+
+try:
+    with open(path, 'r', encoding='utf-8') as f:
+        html_data = f.read()
+        components.html(html_data, height=700, scrolling=True)
+except FileNotFoundError:
+    st.error(f"Erreur : Le fichier {file_name} est introuvable.")
